2b4b2e41170531fbd4660e08da977392a0d30ea7
BugHunter-X <askzenai@gmail.com>
2024-12-13 15:52:06 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,17 @@
+function foo(a, b) {
+  // Explicit type checking
+  if (typeof a === 'number' && typeof b === 'number') {
+    return a + b; 
+  } else {
+    return 'Invalid input: both parameters must be numbers';
+  }
+}
+
+console.log(foo(1, '1')); // Output: Invalid input: both parameters must be numbers
+
+function foo2(a, b) {
+    // Using parseInt
+    return parseInt(a) + parseInt(b);
+}
+
+console.log(foo2(1, '1')); // Output: 2
